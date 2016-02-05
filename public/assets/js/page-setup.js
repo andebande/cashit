277482cc3306961f70d43b1c120f509fecf910ce
andebande <anamaria.cotirlea@gmail.com>
2016-02-05 16:00:08 +0200

    Added the page setup script in an individual file.

---
+++ public/assets/js/page-setup.js
@@ -0,0 +1,122 @@
+  $(document).ready(function()  { 
+            var companyNames =[];
+            var positions = [];
+            var locations = [];
+
+            $.ajax({
+                type: "GET",
+                url: "/data", 
+                success: function (data) {
+                    for(index = 0; index < data.companyNames.length; index++) {
+                        companyNames.push(data.companyNames[index].companyName);
+                    }
+
+                    for(index = 0; index < data.positions.length; index++) {
+                        positions.push(data.positions[index].position);
+                    }
+
+                    for(index = 0; index < data.jobLocations.length; index++) {
+                        locations.push(data.jobLocations[index].location);
+                    }
+                    
+                    $('#searchLocation').autoComplete({
+                        minChars: 1,
+                        source: function (term, suggest) {
+                            term = term.toLowerCase();
+
+                            var suggestions = [];
+                            var choices = locations;
+                           
+                            for (i=0;i<choices.length;i++)
+                                    if (~choices[i].toLowerCase().indexOf(term)) 
+                                        suggestions.push(choices[i]);
+                            
+                            suggest(suggestions);
+                        }
+                    });
+
+                     $('#jobLocation').autoComplete({
+                        minChars: 1,
+                        source: function (term, suggest) {
+                            term = term.toLowerCase();
+
+                            var suggestions = [];
+                            var choices = locations;
+                            
+                            for (i=0;i<choices.length;i++)
+                                    if (~choices[i].toLowerCase().indexOf(term)) 
+                                        suggestions.push(choices[i]);
+                            
+                            suggest(suggestions);
+                        }
+                    });
+
+                    $('#searchPosition').autoComplete({
+                        minChars: 1,
+                        source: function (term, suggest) {
+                            term = term.toLowerCase();
+
+                            var suggestions = [];
+                            var choices = positions;
+                            
+                            for (i=0;i<choices.length;i++)
+                                    if (~choices[i].toLowerCase().indexOf(term)) 
+                                        suggestions.push(choices[i]);
+                            
+                            suggest(suggestions);
+                        }
+                    });
+
+                    $('#position').autoComplete({
+                        minChars: 1,
+                        source: function (term, suggest) {
+                            term = term.toLowerCase();
+
+                            var suggestions = [];
+                            var choices = positions;
+                            
+                            for (i=0;i<choices.length;i++)
+                                    if (~choices[i].toLowerCase().indexOf(term)) 
+                                        suggestions.push(choices[i]);
+                            
+                            suggest(suggestions);
+                        }
+                    });
+
+                    $('#searchCompanyName').autoComplete({
+                        minChars: 1,
+                        source: function (term, suggest) {
+                            term = term.toLowerCase();
+
+                            var suggestions = [];
+                            var choices = companyNames;
+                            
+                            for (i=0;i<choices.length;i++)
+                                    if (~choices[i].toLowerCase().indexOf(term)) 
+                                        suggestions.push(choices[i]);
+                            
+                            suggest(suggestions);
+                        }
+                    });
+
+                    $('#companyName').autoComplete({
+                        minChars: 1,
+                        source: function (term, suggest) {
+                            term = term.toLowerCase();
+
+                            var suggestions = [];
+                            var choices = companyNames;
+                            
+                            for (i=0;i<choices.length;i++)
+                                    if (~choices[i].toLowerCase().indexOf(term)) 
+                                        suggestions.push(choices[i]);
+                            
+                            suggest(suggestions);
+                        }
+                    });
+                }
+            });
+
+            $("#companyTable").tablesorter(); 
+
+}); 
