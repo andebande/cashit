a8f70f4cbcca7044d1ad73c28cb8907f8ce54738
andebande <anamaria.cotirlea@gmail.com>
2016-02-06 22:16:56 +0200

    Disabled submit on enter for forms.

--- public/assets/js/page-setup.js
+++ public/assets/js/page-setup.js
@@ -128,4 +128,19 @@
                 availableTags: []
             });
 
-}); 
+            $('#searchForm').on('keyup keypress', function(e) {
+              var keyCode = e.keyCode || e.which;
+              if (keyCode === 13) { 
+                e.preventDefault();
+                return false;
+              }
+            });
+
+             $('#addForm').on('keyup keypress', function(e) {
+              var keyCode = e.keyCode || e.which;
+              if (keyCode === 13) { 
+                e.preventDefault();
+                return false;
+              }
+            });
+});
